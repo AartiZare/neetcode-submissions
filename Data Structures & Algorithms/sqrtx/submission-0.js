db5ae941347120fc7ae5d788738b7ad23c3e9a20
AartiZare <99880943+AartiZare@users.noreply.github.com>
2026-09-03 15:09:38 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    mySqrt(x) {
+        let left = 1;
+        let right = x;
+        let ans = 0;
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (mid * mid === x) {
+                return mid;
+            } else if (mid * mid < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
