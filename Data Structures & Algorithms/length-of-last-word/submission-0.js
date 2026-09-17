80f40152eb5b7ad0ed6d192d37a60427c6bc5fd5
AartiZare <99880943+AartiZare@users.noreply.github.com>
2026-09-18 00:32:21 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    
+    
+    
+    lengthOfLastWord(s){
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    let count = 0;
+
+    while (i >= 0 && s[i] !== ' ') {
+        count++;
+        i--;
+    }
+
+    return count;
+}
+}
